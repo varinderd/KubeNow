f7060e01e29a3c3fbeccd784fb00257e2e73a148
Anders Larsson <anders.larsson@icm.uu.se>
2017-10-16 14:28:58 +0200

    merged some stuff from dev/cerebellin

--- kvm/main.tf
+++ kvm/main.tf
@@ -2,17 +2,23 @@
 variable cluster_prefix {}
 
 variable kubenow_dir { default = "/tmp" } # to do fixme
-variable kubenow_image { default = "kubenow-v031.qcow2" } # to do fixme
+variable kubenow_image { default = "kubenow-v031-26-g8b8c758-test.qcow2" } # to do fixme
 variable ssh_key { default = "ssh_key.pub"}
 variable ssh_user{ default = "ubuntu"}
 
 variable kubeadm_token {}
+
+variable volume_pool  { default = "default" }
+
+variable network_mode  { default = "nat" }
+variable bridge_name { default = "br0" }
 
 # Master settings
 variable master_count { default = 1 }
 variable master_vcpu { default = 2 }
 variable master_memory { default = 1024 }
 variable master_as_edge { default = "true" }
+variable master_extra_disk_size { default = "200" }
 
 # Nodes settings
 variable node_count { default = 0 }
@@ -29,6 +35,9 @@ variable glusternode_count { default = 0 }
 variable glusternode_vcpu { default = 2 }
 variable glusternode_memory { default = 1024 }
 variable glusternode_extra_disk_size { default = "200" }
+variable gluster_volumetype {
+  default = "none:1"
+}
 
 # Cloudflare settings
 variable use_cloudflare { default = "false" }
@@ -46,7 +55,8 @@ provider "libvirt" {
 # Network
 resource "libvirt_network" "network" {
   name = "${var.cluster_prefix}-network"
-  mode = "nat"
+  mode = "${var.network_mode}"
+#  bridge = "${var.bridge_name}"
   domain = "k8s.local"
   addresses = ["10.0.0.0/16"]
 }
@@ -55,6 +65,7 @@ resource "libvirt_network" "network" {
 resource "libvirt_volume" "template_volume" {
   name   = "${var.cluster_prefix}-template-volume"
   source = "${var.kubenow_dir}/${var.kubenow_image}"
+  pool   = "${var.volume_pool}"
 }
 
 module "master" {
@@ -65,6 +76,7 @@ module "master" {
   vcpu            = "${var.master_vcpu}"
   memory          = "${var.master_memory}"
   template_vol_id = "${libvirt_volume.template_volume.id}"
+  volume_pool     = "${var.volume_pool}"
 
   # Network settings
   network_id = "${libvirt_network.network.id}"
@@ -74,7 +86,7 @@ module "master" {
   # TO DO configure port rules in firewall
 
   # Disk settings
-  extra_disk_size = "0"
+  extra_disk_size = "${var.master_extra_disk_size}"
 
   # Bootstrap settings
   bootstrap_file = "bootstrap/master.sh"
@@ -92,6 +104,7 @@ module "node" {
   vcpu            = "${var.node_vcpu}"
   memory          = "${var.node_memory}"
   template_vol_id = "${libvirt_volume.template_volume.id}"
+  volume_pool     = "${var.volume_pool}"
 
   # Network settings
   network_id = "${libvirt_network.network.id}"
@@ -119,6 +132,7 @@ module "edge" {
   vcpu            = "${var.edge_vcpu}"
   memory          = "${var.edge_memory}"
   template_vol_id = "${libvirt_volume.template_volume.id}"
+  volume_pool     = "${var.volume_pool}"
 
   # Network settings
   network_id = "${libvirt_network.network.id}"
@@ -146,6 +160,7 @@ module "glusternode" {
   vcpu            = "${var.glusternode_vcpu}"
   memory          = "${var.glusternode_memory}"
   template_vol_id = "${libvirt_volume.template_volume.id}"
+  volume_pool     = "${var.volume_pool}"
 
   # Network settings
   network_id = "${libvirt_network.network.id}"
@@ -177,26 +192,28 @@ module "cloudflare" {
   cloudflare_domain = "${var.cloudflare_domain}"
 
   # add cluster prefix to record names
-  record_names = "${formatlist("%s.%s", var.cloudflare_record_texts, var.cluster_prefix)}"
+  # terraform interpolation is limited and can not return list in conditionals, workaround: first join to string, then split
+  record_names = "${split(",", var.cloudflare_proxied == true ? join(",", formatlist("%s-%s", var.cloudflare_record_texts, var.cluster_prefix) ) : join(",", formatlist("%s.%s", var.cloudflare_record_texts, var.cluster_prefix)))}"
 
-  # terraform interpolation is limited and can not return list in conditionals, workaround: first join to string, then split)
+  # terraform interpolation is limited and can not return list in conditionals, workaround: first join to string, then split
   iplist  = "${split(",", var.master_as_edge == true ? join(",", concat(module.edge.public_ip, module.master.public_ip) ) : join(",", module.edge.public_ip) )}"
   proxied = "${var.cloudflare_proxied}"
 }
 
 # Generate Ansible inventory (identical for each cloud provider)
 module "generate-inventory" {
-  source            = "../common/inventory"
-  master_hostnames  = "${module.master.hostnames}"
-  master_public_ip  = "${module.master.public_ip}"
-  edge_hostnames    = "${module.edge.hostnames}"
-  edge_public_ip    = "${module.edge.public_ip}"
-  master_as_edge    = "${var.master_as_edge}"
-  edge_count        = "${var.edge_count}"
-  node_count        = "${var.node_count}"
-  glusternode_count = "${var.glusternode_count}"
-  extra_disk_device = "${element(concat(module.glusternode.extra_disk_device, list("")),0)}"
-  cluster_prefix    = "${var.cluster_prefix}"
-  use_cloudflare    = "${var.use_cloudflare}"
-  cloudflare_domain = "${var.cloudflare_domain}"
+  source             = "../common/inventory"
+  master_hostnames   = "${module.master.hostnames}"
+  master_public_ip   = "${module.master.public_ip}"
+  edge_hostnames     = "${module.edge.hostnames}"
+  edge_public_ip     = "${module.edge.public_ip}"
+  master_as_edge     = "${var.master_as_edge}"
+  edge_count         = "${var.edge_count}"
+  node_count         = "${var.node_count}"
+  glusternode_count  = "${var.glusternode_count}"
+  gluster_volumetype = "${var.gluster_volumetype}"
+  extra_disk_device  = "${element(concat(module.glusternode.extra_disk_device, list("")),0)}"
+  cluster_prefix     = "${var.cluster_prefix}"
+  use_cloudflare     = "${var.use_cloudflare}"
+  cloudflare_domain  = "${var.cloudflare_proxied == true ? var.cloudflare_domain : format("%s.%s", var.cluster_prefix, var.cloudflare_domain)}"
 }

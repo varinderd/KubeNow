afa79b1a70d0ffc0bba7ff36afd65a8ef54f165d
Anders Larsson <anders.larsson@icm.uu.se>
2017-11-18 12:01:52 +0100

    only started editing - not working yet [skip ci]

--- common/inventory/main.tf
+++ common/inventory/main.tf
@@ -48,65 +48,58 @@ variable glusternode_count {}
 variable gluster_volumetype {}
 variable extra_disk_device {}
 
-# Generate Ansible inventory (identical for each cloud provider)
-resource "null_resource" "generate-inventory" {
+variable bastion_hostnames {
+  type = "list"
+}
+
+variable bastion_public_ip {
+  type = "list"
+}
+
+variable bastion_labels {
+  type = "list"
+}
+
+variable inventory_template_file {
+  default = "inventory-template"
+}
+
+variable inventory_output_file {
+  default = "inventory"
+}
+
+# Generate inventory from template file
+data "template_file" "inventory" {
+  template = "${file("${path.root}/../${ var.inventory_template_file }")}"
+
+  vars {
+    masters = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s", var.master_hostnames , var.master_public_ip, var.ssh_user))}"
+    
+    nodes = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s", var.node_hostnames , var.node_public_ip, var.ssh_user))}"
+
+    only_edges = "${var.edge_count == 0 ? " " : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", slice(var.edge_hostnames,0,var.edge_count), var.edge_public_ip))}"
+    
+    # Add master to edges if that is the case
+    edges = ${var.master_as_edge != true ? var.edges : "${format("%s\n%s", var.masters, var.edges)}
+    
+    ansible_ssh_user         = "${var.ssh_user}"
+    master-hostname-private  = "${element(concat(var.master_hostnames, list("")),0)}"
+    master_hostname_public   = "${var.domain}"
+    master_default_subdomain = "${var.domain}"
+  }
+}
+
+resource "null_resource" "local" {
   # Trigger rewrite of inventory, uuid() generates a random string everytime it is called
   triggers {
     uuid = "${uuid()}"
   }
 
-  # Write master
-  provisioner "local-exec" {
-    command = "echo \"[master]\" > inventory"
-  }
-
-  # output the lists formated
-  provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", var.master_hostnames, var.master_public_ip))}\" >> inventory"
-  }
-
-  # Write edges
-  provisioner "local-exec" {
-    command = "echo \"[edge]\" >> inventory"
-  }
-
-  # only output if master is edge
-  provisioner "local-exec" {
-    command = "echo \"${var.master_as_edge != true ? "" : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", var.master_hostnames, var.master_public_ip))}\" >> inventory"
-  }
-
-  # output the lists formated, slice list to make sure hostname and ip-list have same length
-  # provisioner output can not be empty string - therefore output space when edge_count == 0
-  provisioner "local-exec" {
-    command = "echo \"${var.edge_count == 0 ? " " : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", slice(var.edge_hostnames,0,var.edge_count), var.edge_public_ip))}\" >> inventory"
-  }
-
-  # Write other variables
-  provisioner "local-exec" {
-    command = "echo \"[all:vars]\" >> inventory"
+  triggers {
+    template = "${data.template_file.inventory.rendered}"
   }
 
   provisioner "local-exec" {
-    command = "echo \"nodes_count=${1 + var.edge_count + var.node_count + var.glusternode_count} \" >> inventory"
-  }
-
-  # If cloudflare domain is set, output that domain, otherwise output a nip.io domain (with the first edge ip)
-  provisioner "local-exec" {
-    command = "echo \"domain=${ var.domain }\" >> inventory"
-  }
-
-  # Always output extra disk device
-  provisioner "local-exec" {
-    command = "echo \"extra_disk_device=${var.extra_disk_device}\" >> inventory"
-  }
-
-  # Always output glusternode count
-  provisioner "local-exec" {
-    command = "echo \"glusternode_count=${var.glusternode_count}\" >> inventory"
-  }
-
-  # Always output gluster_volumetype
-  provisioner "local-exec" {
-    command = "echo \"gluster_volumetype=${var.gluster_volumetype}\" >> inventory"
+    command = "echo -e '${data.template_file.inventory.rendered}' > \"${path.root}/../${var.inventory_output_file}\""
   }
 }
